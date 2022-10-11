75c0d3d9a94908ae836bb41db4f250c0233885b3
slfrnkln <sol@franklincentral.co.uk>
2022-10-11 18:04:39 +0100

    Updated Pipfile and use of .env

--- app/database.py
+++ app/database.py
@@ -2,7 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/bamboobank"
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+DB_URL = os.environ.get("DATABASE_URL")
+DB_NAME = os.environ.get("DATABASE_NAME")
+DB_USER = os.environ.get("DATABASE_USER")
+DB_PWORD = os.environ.get("DATABASE_PWORD")
+
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/bamboobank"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PWORD}@{DB_URL}/{DB_NAME}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
